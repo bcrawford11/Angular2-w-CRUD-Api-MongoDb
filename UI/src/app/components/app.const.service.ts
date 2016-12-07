a39fe74d11cf11d781a11af7e308ef66e72650ca
bcrawford11 <vwjettakid@gmail.com>
2016-12-07 07:36:51 -0800

    Update app.const.service.ts

--- UI/src/app/components/app.const.service.ts
+++ UI/src/app/components/app.const.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class Constants{
     public Server: string = "http://localhost:5000/";
-    public ApiUrl: string = "api/Post";
+    public ApiUrl: string = "api/Post/";
     public ServerWithApi = this.Server + this.ApiUrl;
 }
